76877735637161b9208e50c3e33d8250081627db
mcramileux <kristineramilo21@gmail.com>
2023-08-05 08:08:15 +1000

    db fixed 2.0 still getting errors

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,8 +16,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // Export a function we will use to POST to the database.
-export const postDb = async (content)  => {
-  console.error('putDb not implemented');
+export const putDb = async (content)  => {
   console.log('PUT to the database');
 
   // Create a connection to the database database and version we want to use.
